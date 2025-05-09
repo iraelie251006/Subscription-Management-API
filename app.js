3d73382ba77dbf9361b1591b13aa324cc2801bd9
Elie <niyubwayoiraelie5777@gmail.com>
2025-05-09 23:06:06 +0200

    Setting Up MongoDB Database

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import { PORT } from "./config/env.js";
 import userRouter from "./routes/user.routes.js";
 import authRouter from "./routes/auth.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
+import connectToDatabase from "./databse/mongodb.js";
 
 const app = express();
 
@@ -15,8 +16,10 @@ app.get("/", (req, res) => {
     res.send("Welcome to SubDub API")
 })
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`)
+app.listen(PORT, async () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+
+    await connectToDatabase();
 })
 
 export default app
