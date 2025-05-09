23361daccd197814e5ae17e47186b200e2345650
Elie <niyubwayoiraelie5777@gmail.com>
2025-05-09 10:58:56 +0200

    Implementing API Configuration

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 import express from "express";
 
+import { PORT } from "./config/env.js";
+
 const app = express();
 
 app.get("/", (req, res) => {
     res.send("Welcome to SubDub API")
 })
 
-app.listen(4000, () => {
-    console.log("Server is running on http://localhost:4000")
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`)
 })
 
 export default app
