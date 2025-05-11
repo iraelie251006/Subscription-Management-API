3e71546f4c703264b8fa2595fca732f38c86e731
Elie <niyubwayoiraelie5777@gmail.com>
2025-05-11 18:19:50 +0200

    Implement Global Error Handling.

--- app.js
+++ app.js
@@ -5,12 +5,19 @@ import userRouter from "./routes/user.routes.js";
 import authRouter from "./routes/auth.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
 import connectToDatabase from "./databse/mongodb.js";
+import { errorMiddleware } from "./middlewares/error.middleware.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/subscriptions', subscriptionRouter);
+
+app.use(errorMiddleware);
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser())
 
 app.get("/", (req, res) => {
     res.send("Welcome to SubDub API")
