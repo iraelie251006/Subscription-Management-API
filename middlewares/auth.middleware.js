7fbd54860bcb44cc2391f79e2a8819975de95c8a
Elie <niyubwayoiraelie5777@gmail.com>
2025-05-12 00:09:06 +0200

    Bug fixes: Correct typos while importing modules

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -1,6 +1,6 @@
 import jwt from 'jsonwebtoken';
-import { JWT_SECRET } from '../config/env';
-import User from '../models/user.model';
+import { JWT_SECRET } from '../config/env.js';
+import User from '../models/user.model.js';
 
 const authorize = async(req, res, next) => {
     try {
