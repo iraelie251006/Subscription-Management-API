5d31761ce158eb8524df9f09c8f7574f7cb51b5d
Elie <niyubwayoiraelie5777@gmail.com>
2025-05-13 23:10:35 +0200

    Implement Reminders Workflow

--- app.js
+++ app.js
@@ -8,12 +8,14 @@ import connectToDatabase from "./database/mongodb.js";
 import { errorMiddleware } from "./middlewares/error.middleware.js";
 import cookieParser from "cookie-parser";
 import arcjetMiddleware from "./middlewares/arcjet.middleware.js";
+import workflowRouter from "./routes/workflow.routes.js";
 
 const app = express();
 
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/subscriptions', subscriptionRouter);
+app.use('/api/v1/workflow', workflowRouter);
 
 app.use(errorMiddleware);
 app.use(express.json());
