f121f2b6a496655605d7850b935697c928990cd5
Elie <niyubwayoiraelie5777@gmail.com>
2025-05-15 23:10:12 +0200

    Removed unused routes in auth.routes.js and subscription.routes.js

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -7,6 +7,4 @@ authRouter.post("/sign-in", signIn);
 
 authRouter.post("/sign-up", signUp);
 
-// authRouter.post("/sign-out", signOut);
-
 export default authRouter;
--- routes/subscription.routes.js
+++ routes/subscription.routes.js
@@ -10,14 +10,4 @@ subscriptionRouter.get("/:id", authorize, getUserSubscriptions);
 
 subscriptionRouter.post("/", authorize, createSubscription);
 
-subscriptionRouter.put("/:id", (req, res) => res.send({title: 'UPDATE subscription'}));
-
-subscriptionRouter.delete("/:id", (req, res) => res.send({title: 'DELETE subscription'}));
-
-subscriptionRouter.get("/user/:id", (req, res) => res.send({title: 'GET all subscriptions for user'}));
-
-subscriptionRouter.put("/:id/cancel", (req, res) => res.send({title: 'CANCEL subscription'}));
-
-subscriptionRouter.get("/upcoming-renewals", (req, res) => res.send({title: 'GET upcoming renewals'}));
-
 export default subscriptionRouter;
